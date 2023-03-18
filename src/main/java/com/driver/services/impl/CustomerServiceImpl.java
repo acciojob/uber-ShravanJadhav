45c396b05f1bf3300eacd1b958de884ac816aa5e
ShravanJadhav <shravanjadhav844@gmail.com>
2023-03-18 15:41:04 +0530

    cab_Changes

--- src/main/java/com/driver/services/impl/CustomerServiceImpl.java
+++ src/main/java/com/driver/services/impl/CustomerServiceImpl.java
@@ -48,7 +48,7 @@ public class CustomerServiceImpl implements CustomerService {
 		Driver driver=null;
 		List<Driver>driverList = driverRepository2.findAll();
 		for(Driver driver1: driverList){
-			if(driver1.getCab().isAvailable()){
+			if(driver1.getCab().getAvailable()){
 				if((driver==null)||(driver.getDriverId()>driver1.getDriverId())){
 					driver=driver1;
 				}
